a0d8e10b3d7654c53dc0bd5a7d97a5da67be3d6f
VanyaLyashuk <ivan129522@gmail.com>
2023-11-16 09:46:33 +0200

    fixed: you can see part of header logo when the navigation is open

--- src/components/nav/NavMob.tsx
+++ src/components/nav/NavMob.tsx
@@ -1,19 +1,23 @@
 import classNames from "classnames";
-import { useAppSelector } from '../../hooks/hook';
+import { useAppSelector } from "../../hooks/hook";
 import Logo from "../UI/Logo";
-import NavDesk from './NavDesk';
-import NavList from './NavList';
+import NavDesk from "./NavDesk";
+import NavList from "./NavList";
 
 const NavMob = () => {
-  const {isOpened} = useAppSelector(state => state.navigation);
+  const { isOpened } = useAppSelector((state) => state.navigation);
 
   const navClasses = classNames(
     "pt-5 landscape:pb-[80px] w-0 bg-white transition-all duration-300 overflow-x-hidden overflow-y-scroll  absolute h-full let-0 top-0 lg:hidden flex flex-col items-center z-20",
     {
-      "w-[270px] md:w-[300px] transition-all border-r-4 border-black scrollbar-hide landscape:scrollbar-default": isOpened,
+      "w-[270px] md:w-[320px] transition-all border-r-4 border-black scrollbar-hide landscape:scrollbar-default":
+        isOpened,
     }
   );
-  const navLogoClasses = classNames("sticky z-50 bg-white top-0 opacity-0 mb-5 transition-opacity delay-[55ms]", {"opacity-100": isOpened});
+  const navLogoClasses = classNames(
+    "sticky z-50 bg-white top-0 opacity-0 mb-5 transition-opacity delay-[55ms]",
+    { "opacity-100": isOpened }
+  );
 
   return (
     <nav className={navClasses}>
