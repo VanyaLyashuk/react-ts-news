36c1ca6ba0e4fd42a7064e488d3fb03ec6124213
VanyaLyashuk <ivan129522@gmail.com>
2023-11-28 19:09:03 +0200

    add margin to news grid & change title font size

--- src/components/pages/NewsGrid.tsx
+++ src/components/pages/NewsGrid.tsx
@@ -7,12 +7,12 @@ interface INewsGrid {
 }
 
 const NewsGrid = ({title}: INewsGrid) => {
-  const titleClasses = "inline-flex mb-4 relative text-4xl font-bold lg:text-5xl lg:mb-6 before:content-[''] before:bg-[#add8e6] before:w-[101%] before:h-3 before:left-0 before:bottom-[1px] min-[370px]:before:bottom-[1px] min-[370px]:before:h-3 before:absolute before:z-[-1] before:transition-all before:duration-500";
+  const titleClasses = "inline-flex mb-4 relative text-4xl font-bold lg:text-[40px] xl:text-[46px] lg:mb-6 before:content-[''] before:bg-[#add8e6] before:w-[101%] before:h-3 before:left-0 before:bottom-[1px] min-[370px]:before:bottom-[1px] min-[370px]:before:h-3 before:absolute before:z-[-1] before:transition-all before:duration-500";
   return (
     <div className="container mx-auto">
       <div className='w-full pt-10 pb-[60px] max-w-[420px] sm:max-w-full m-auto lg:pt-16 lg:pb-[100px]'>        
         <h2 className={titleClasses}>{title}</h2>
-        <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
+        <div className='grid gap-4 mb-7 md:mb-10 lg:mb-12 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
           <NewsCard />
           <NewsCard />
           <NewsCard />
